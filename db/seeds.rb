c6e87da6c48ceccea3eae7559f72c044b6a500dd
Jess <jessicatanhc@gmail.com>
2020-02-18 16:31:10 +0800

    Add ffaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'ffaker'
+
+user = User.create(:username=>'admin@email.com', :email=>"password123");
+
+10.times do
+ User.create(
+        email: FFaker::Internet.email,
+        password: 'password123',
+      )
+end
