c184729c9291f1b7de27247ecd6e56160b64d0df
Jess <jessicatanhc@gmail.com>
2020-02-23 15:23:35 +0800

    Add validation for creating profile

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -4,27 +4,39 @@ class SittersController < ApplicationController
       def index
           @sitters = Sitter.where(:is_visible => true).where.not(:user => current_user)
           @pets = Pet.all
-          
+          @a_date = ""
       end
+
       def search
-          #referring to sitters that matches time specified
-          @sitters_group_1 = Sitter.all
-          @sitters_group_2 = Sitter.all
+        @sitters_group_1 = Sitter.where(:is_visible => true).where.not(:user => current_user)
+        @sitters_group_2 = Sitter.where(:is_visible => true).where.not(:user => current_user)
         #find sitters group 1 if neither dates are empty
         if search_params[:available_start_date] != "" &&search_params[:available_end_date]
-        @timeslot = Timeslot.where("available_start_date <= ? and available_end_date >= ?",search_params[:available_start_date],search_params[:available_end_date])
-          @sitters_group_1 = @timeslot.map { |timeslot|
-            timeslot.sitter
-          }
+            @timeslot = Timeslot.where("available_start_date <= ? and available_end_date >= ?",search_params[:available_start_date],search_params[:available_end_date])
+            @sitters_group_1 = @timeslot.map { |timeslot|
+                timeslot.sitter
+              }
         end
         #find sitters group 2 if pet not empty
         if search_params[:pet_id] != ""
           @pet = Pet.find(search_params[:pet_id])
           @sitters_group_2 = @pet.sitters
         end
-          @sitters = (@sitters_group_1 & @sitters_group_2).uniq
+          @sitters_group_3 = (@sitters_group_1 & @sitters_group_2).uniq
+          @sitters = @sitters_group_3.select do |sitter|
+              sitter.is_visible == true && sitter.user != current_user
+          end
+          @a_date = search_params[:available_start_date]
           @pets = Pet.all
           render "index"
+      end
+
+      def profile
+        @sitter = Sitter.find_by(user: current_user)
+        if @sitter
+        else
+          redirect_to new_profile_path
+        end
       end
       def profile
         @sitter = Sitter.find_by(user: current_user)
@@ -47,6 +59,9 @@ class SittersController < ApplicationController
         @timeslot = Timeslot.new(timeslot_params)
         @sitter.user = current_user
 
+        if(@sitter.pet_ids.length == 0)
+          @sitter.pets << Pet.all
+        end
         # empty checkboxes are not sent, therefore nil
         if !@sitter.is_visible
           @sitter.is_visible = false
@@ -97,9 +112,10 @@ class SittersController < ApplicationController
       def sitter_params
         params.require(:sitter).permit(:phone, :description, :location, :price, :about, :picture, :is_visible, :pet_ids=>[])
       end
-
+      def timeslot_params
+        params.require(:sitter).permit(:available_start_date, :available_end_date)
+      end
       def search_params
         params.require(:sitter).permit(:available_start_date, :available_end_date, :pet_id)
       end
-
 end
