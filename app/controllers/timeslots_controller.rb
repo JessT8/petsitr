6e1e2dd39eca4e4b136a40a6fd46fae12e5cd634
SamTanFL <samsite_tan@hotmail.com>
2020-02-24 15:06:18 +0800

    added condition to show flash msg

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -13,21 +13,17 @@ class TimeslotsController < ApplicationController
         #check if timeslot has already been exists
         timeslot_check = Timeslot.where(sitter: current_user.sitter)
         if timeslot_check.length > 0
-            flash.alert = "Timeslots already exist"
-            redirect_to profile_path and return
+            redirect_to profile_path, flash: { error: "Timeslots already exist" } and return
         end
         @timeslot = Timeslot.new(timeslot_params)
 
         @timeslot.sitter = current_user.sitter
         if @timeslot.available_start_date == nil || @timeslot.available_end_date == nil
-            flash.alert = "Timeslot Fields Empty"
-            redirect_to new_timeslot_path
+            redirect_to new_timeslot_path, flash: { error: "Timeslot Fields Empty" }
         else
             bookings = Booking.where(status: true).where(sitter: current_user.sitter).where.not('start_date > ? AND end_date > ?', @timeslot.available_end_date, @timeslot.available_end_date).where.not('start_date < ? AND end_date < ?', @timeslot.available_start_date, @timeslot.available_start_date)
-            byebug
             if bookings.length == 0
                 if @timeslot.save
-                    flash.notice = "Timeslot Created"
                     redirect_to profile_path
                 else
                     redirect_to new_timeslot_path, flash: { error: "Something went wrong" }
@@ -43,15 +39,20 @@ class TimeslotsController < ApplicationController
     end
 
     def update
-        booking_check = Timeslot.new(timeslot_params)
-        bookings = Booking.where(status: true).where(sitter: current_user.sitter).where.not('start_date > ? AND end_date > ?', booking_check.available_end_date, booking_check.available_end_date).where.not('start_date < ? AND end_date < ?', booking_check.available_start_date, booking_check.available_start_date)
+        checks = Timeslot.new(timeslot_params)
+        bookings = Booking.where(status: true).where(sitter: current_user.sitter).where.not('start_date > ? AND end_date > ?', checks.available_end_date, checks.available_end_date).where.not('start_date < ? AND end_date < ?', checks.available_start_date, checks.available_start_date)
         if bookings.length == 0
-            if timeslot_params[:available_start_date] < timeslot_params[:available_end_date]
-                @timeslot = Timeslot.find(params[:id])
-                @timeslot.update(timeslot_params)
-                redirect_to profile_path
+            timeslots = Timeslot.where(sitter: current_user.sitter).where.not(id: params[:id]).where.not('available_start_date > ? AND available_end_date > ?', checks.available_end_date, checks.available_end_date).where.not('available_start_date < ? AND available_end_date < ?', checks.available_start_date, checks.available_start_date)
+            if timeslots.length == 0
+                if timeslot_params[:available_start_date] <= timeslot_params[:available_end_date]
+                    @timeslot = Timeslot.find(params[:id])
+                    @timeslot.update(timeslot_params)
+                    redirect_to profile_path
+                else
+                    redirect_to profile_path, flash: { error: "End Must Be Later Than Start Date" }
+                end
             else
-                redirect_to profile_path, flash: { error: "End Must Be Later Than Start Date" }
+                redirect_to profile_path, flash: { error: "Timeslot Conflicts with Existing Timeslot"}
             end
         else
             redirect_to profile_path, flash: { error: "Confirmed Booking Exists in Same Timeslot" }
